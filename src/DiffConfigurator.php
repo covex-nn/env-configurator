c04182131e4ea057cb165a4314549368800b27be
Andrey F. Mindubaev <covex.mobile@gmail.com>
2018-05-11 19:05:46 +0300

    Diff configurator

---
+++ src/DiffConfigurator.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Andrey F. Mindubaev <covex.mobile@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Covex\Environment\Configurator;
+
+class DiffConfigurator extends ConfiguratorAbstract
+{
+    public function apply(): void
+    {
+        $target = $this->getTarget();
+        $data = [];
+        foreach (file($target) as $key => $value) {
+            $value = trim($value);
+            if (strlen($value)) {
+                $data[] = $value;
+            }
+        }
+        sort($data);
+
+        $diff = file($this->getSource());
+        foreach ($diff as $line) {
+            $line = trim($line);
+            $action = substr($line, 0, 1);
+            $line = trim(substr($line, 1));
+
+            if ('-' === $action) {
+                if (!strlen($line)) {
+                    $data = [];
+                } else {
+                    $data = array_filter($data, function ($value) use ($line) {
+                        return $value !== $line;
+                    });
+                }
+            } elseif ('+' === $action) {
+                if (!in_array($line, $data)) {
+                    $data[] = $line;
+                    sort($data);
+                }
+            } else {
+                throw new ConfiguratorException('Diff lines can start with - or + only');
+            }
+        }
+
+        $content = implode(PHP_EOL, $data);
+        if (count($data)) {
+            $content .= PHP_EOL;
+        }
+        file_put_contents($target, $content);
+    }
+}
---
+++ tests/DiffConfiguratorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Andrey F. Mindubaev <covex.mobile@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Covex\Environment\Configurator\Tests;
+
+use Covex\Environment\Configurator\ConfiguratorException;
+use Covex\Environment\Configurator\DiffConfigurator;
+
+class DiffConfiguratorTest extends ConfiguratorTestCase
+{
+    public function testError(): void
+    {
+        $this->expectException(ConfiguratorException::class);
+        $this->expectExceptionMessage('Diff lines can start with - or + only');
+
+        $configurator = new DiffConfigurator();
+        $configurator
+            ->setSource('vfs://source.error.txt')
+            ->setTarget('vfs://target.txt')
+            ->apply();
+    }
+
+    public function testClear(): void
+    {
+        $configurator = new DiffConfigurator();
+        $configurator
+            ->setSource('vfs://source.clear.txt')
+            ->setTarget('vfs://target.txt')
+            ->apply();
+
+        $this->assertEquals('', file_get_contents('vfs://target.txt'));
+    }
+
+    public function testRemove(): void
+    {
+        $configurator = new DiffConfigurator();
+        $configurator
+            ->setSource('vfs://source.remove.txt')
+            ->setTarget('vfs://target.txt')
+            ->apply();
+
+        $this->assertEquals('password=asdf'.PHP_EOL, file_get_contents('vfs://target.txt'));
+    }
+
+    public function testAdd(): void
+    {
+        $configurator = new DiffConfigurator();
+        $configurator
+            ->setSource('vfs://source.add.txt')
+            ->setTarget('vfs://target.txt')
+            ->apply();
+
+        $content = implode(PHP_EOL, [
+            '.env', 'asdfgh', 'login=qwerty', 'password=asdf', '',
+        ]);
+        $this->assertEquals($content, file_get_contents('vfs://target.txt'));
+    }
+
+    protected function getVfsRoot(): string
+    {
+        return __DIR__.DIRECTORY_SEPARATOR.'diff';
+    }
+}
